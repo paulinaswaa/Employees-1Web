8e044749dfe0a0550e0db83277f31c3532f6100f
paulinaswaa <124894941+paulinaswaa@users.noreply.github.com>
2023-04-15 20:51:20 +0200

    Add to list

--- EmployeeList/EmployeeList/Pages/Employees/Index.cshtml.cs
+++ EmployeeList/EmployeeList/Pages/Employees/Index.cshtml.cs
@@ -1,12 +1,34 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data.SqlClient;
 
 namespace EmployeeList.Pages.Employees
 {
     public class IndexModel : PageModel
     {
+        public List<EmployeesInfo> ListEmployees = new List<EmployeesInfo>();
         public void OnGet()
         {
+            try
+            {
+                String connectionString = "Data Source=DESKTOP-HR20LU4;Initial Catalog=ListOfEmployees;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+                using (SqlConnection sqlConnection = new SqlConnection(connectionString)) 
+                {
+                    sqlConnection.Open(); 
+                }
+            }
+            catch(Exception ex) { 
+            
+            }
+        }
+
+        public class EmployeesInfo
+        {
+            public string id;
+            public string FirstName;
+            public string LastName;
+            public string Email;
+            public string Phone;
         }
     }
 }
