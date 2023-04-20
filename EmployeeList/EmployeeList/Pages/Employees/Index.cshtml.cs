6eb20b1477b2e603c327846c55646f110e63cbd9
paulinaswaa <124894941+paulinaswaa@users.noreply.github.com>
2023-04-20 11:03:55 +0200

    Display list of employees

--- EmployeeList/EmployeeList/Pages/Employees/Index.cshtml.cs
+++ EmployeeList/EmployeeList/Pages/Employees/Index.cshtml.cs
@@ -1,33 +1,42 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Data.SqlClient;
+using System.Collections.Generic;
+using static System.Net.Mime.MediaTypeNames;
+using System;
 
 namespace EmployeeList.Pages.Employees
 {
     public class IndexModel : PageModel
     {
         public List<EmployeesInfo> ListEmployees = new List<EmployeesInfo>();
+
         public void OnGet()
         {
             try
             {
-                String connectionString = "Data Source=DESKTOP-HR20LU4;Initial Catalog=ListOfEmployees;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
-                using (SqlConnection sqlConnection = new SqlConnection(connectionString)) 
+                //SqlConnection connectionString = new SqlConnection(@"Data Source=DESKTOP-HR20LU4;Initial Catalog=ListOfEmployees;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+                //String ConnectionString = "Data Source=DESKTOP-HR20LU4;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+
+                String ConnectionString = "Server=DESKTOP-HR20LU4; Database=ListOfEmployees; Trusted_Connection=true;";
+
+
+                using (SqlConnection sqlConnection = new SqlConnection(ConnectionString))
                 {
                     sqlConnection.Open();
                     String sql = "SELECT * FROM Employee";
-                    using(SqlCommand cmd = new SqlCommand(sql, sqlConnection))
+                    using (SqlCommand cmd = new SqlCommand(sql, sqlConnection))
                     {
-                        using(SqlDataReader reader = cmd.ExecuteReader())
+                        using (SqlDataReader reader = cmd.ExecuteReader())
                         {
                             while (reader.Read())
                             {
                                 EmployeesInfo info = new EmployeesInfo();
-                                info.id = " " +reader.GetInt32(0);
+                                info.id = reader.GetInt32(0).ToString();
                                 info.FirstName = reader.GetString(1);
-                                info.LastName = reader.GetString(2);    
-                                info.Email = reader.GetString(3);  
-                                info.Phone= reader.GetString(4);
+                                info.LastName = reader.GetString(2);
+                                info.Email = reader.GetString(3);
+                                info.Phone = reader.GetString(4);
 
                                 ListEmployees.Add(info);
                             }
@@ -35,8 +44,9 @@ namespace EmployeeList.Pages.Employees
                     }
                 }
             }
-            catch(Exception ex) { 
-                Console.WriteLine("exception: "+ ex.ToString());
+            catch (Exception ex)
+            {
+                Console.WriteLine("exception: " + ex.ToString());
             }
         }
 
---
+++ EmployeeList/EmployeeList/Pages/Employees/add.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EmployeeList.Pages.Employees
+{
+    public class addModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
