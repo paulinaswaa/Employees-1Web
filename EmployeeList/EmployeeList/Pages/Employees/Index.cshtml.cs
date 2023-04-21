f561d99e3b2000b3793373636cc18d08b80aa2c7
paulinaswaa <124894941+paulinaswaa@users.noreply.github.com>
2023-04-21 23:58:37 +0200

    Add new employee

--- EmployeeList/EmployeeList/Pages/Employees/Index.cshtml.cs
+++ EmployeeList/EmployeeList/Pages/Employees/Index.cshtml.cs
@@ -4,20 +4,19 @@ using System.Data.SqlClient;
 using System.Collections.Generic;
 using static System.Net.Mime.MediaTypeNames;
 using System;
+using System.Reflection;
 
 namespace EmployeeList.Pages.Employees
 {
     public class IndexModel : PageModel
     {
         public List<EmployeesInfo> ListEmployees = new List<EmployeesInfo>();
+        
 
         public void OnGet()
         {
             try
             {
-                //SqlConnection connectionString = new SqlConnection(@"Data Source=DESKTOP-HR20LU4;Initial Catalog=ListOfEmployees;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
-                //String ConnectionString = "Data Source=DESKTOP-HR20LU4;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
-
                 String ConnectionString = "Server=DESKTOP-HR20LU4; Database=ListOfEmployees; Trusted_Connection=true;";
 
 
@@ -32,7 +31,7 @@ namespace EmployeeList.Pages.Employees
                             while (reader.Read())
                             {
                                 EmployeesInfo info = new EmployeesInfo();
-                                info.id = reader.GetInt32(0).ToString();
+                                info.id = "" + reader.GetInt32(0);
                                 info.FirstName = reader.GetString(1);
                                 info.LastName = reader.GetString(2);
                                 info.Email = reader.GetString(3);
@@ -50,13 +49,15 @@ namespace EmployeeList.Pages.Employees
             }
         }
 
-        public class EmployeesInfo
-        {
-            public string id;
-            public string FirstName;
-            public string LastName;
-            public string Email;
-            public string Phone;
-        }
+      
+    }
+
+    public class EmployeesInfo
+    {
+        public string id;
+        public string FirstName;
+        public string LastName;
+        public string Email;
+        public string Phone;
     }
 }
--- EmployeeList/EmployeeList/Pages/Employees/add.cshtml.cs
+++ EmployeeList/EmployeeList/Pages/Employees/add.cshtml.cs
@@ -1,12 +1,73 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data.SqlClient;
 
 namespace EmployeeList.Pages.Employees
 {
     public class addModel : PageModel
     {
+        public EmployeesInfo employeesInfo = new EmployeesInfo();
+        public string errorMessage = ""; 
+        public string correctMessage = "";
+
         public void OnGet()
         {
         }
+
+        public void OnPost()
+        {
+            employeesInfo.FirstName = Request.Form["FirstName"];
+            employeesInfo.LastName = Request.Form["LastName"];
+            employeesInfo.Email = Request.Form["Email"];
+            employeesInfo.Phone = Request.Form["Phone"];
+
+
+            if (employeesInfo.Email.Length == 0 || employeesInfo.Phone.Length == 0 ||
+                employeesInfo.LastName.Length == 0 || employeesInfo.LastName.Length == 0)
+            {
+                errorMessage = "Complete the data!";
+                return;
+            }
+
+            try
+            {
+                String ConnectionString = "Server=DESKTOP-HR20LU4; Database=ListOfEmployees; Trusted_Connection=true;";
+
+
+                using (SqlConnection sqlConnection = new SqlConnection(ConnectionString))
+                {
+                    sqlConnection.Open();
+                    String sql = "INSERT INTO Employee(FirstName, LastName, Email, Phone) VALUES (@firstName, @lastName, @email, @phone)";
+
+                    using (SqlCommand sqlCommand = new SqlCommand(sql, sqlConnection))
+                    {
+                        sqlCommand.Parameters.AddWithValue(@"firstName", employeesInfo.FirstName);
+                        sqlCommand.Parameters.AddWithValue(@"lastName", employeesInfo.LastName);
+                        sqlCommand.Parameters.AddWithValue(@"email", employeesInfo.Email);
+                        sqlCommand.Parameters.AddWithValue(@"phone", employeesInfo.Phone);
+                        sqlCommand.ExecuteNonQuery();
+
+
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                errorMessage = ex.Message;
+                return;
+            }
+            employeesInfo.FirstName = "";
+            employeesInfo.LastName = "";
+            employeesInfo.Email = "";
+            employeesInfo.Phone = "";
+            correctMessage = "Added correctly!";
+
+            Response.Redirect("/Employees/Index");
+        }
+
+
+            
+            
     }
 }
